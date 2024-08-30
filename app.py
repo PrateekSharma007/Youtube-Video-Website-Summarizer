83a2d2d3a9cc7a6483a2eaa095f4eda536d818fb
PrateekSharma007 <btech.m2_66995@apsc.mait.ac.in>
2024-08-31 03:53:51 +0530

    groq

--- app.py
+++ app.py
@@ -3,4 +3,34 @@ import streamlit as st
 from langchain.prompts import PromptTemplate 
 from langchain_groq import ChatGroq
 from langchain.chains.summarize import load_summarize_chain
+from langchain_community.document_loaders import YoutubeLoader, UnstructuredURLLoader 
 
+
+##streamlit app 
+
+st.set_page_config(page_title="Youtube/Website Summarize") 
+st.title("Youtube/Website Summarize")
+st.subheader("Summarize URL") 
+
+
+##groq api key 
+
+with st.sidebar: 
+    groq_api_key= st.text_input("Groq API KEY" , value="password") 
+
+
+url = st.text_input("URL",label_visibility="collapsed")
+
+
+
+if st.button("Summarize the content from the URL") : 
+    if not groq_api_key.strip() or not url.strip() : 
+        st.error("Please provide the information")
+    elif not validators.url(url) : 
+        st.error("Please provide a valid URL.")
+
+    else: 
+        try : 
+            with st.spinner("waiting...") : 
+                 
+    
